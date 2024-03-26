a832a698ef916c4f0bc8c977b294346ead50bd84
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-26 12:53:05 -0400

    Prompt function created with choices for user to select from

--- index.js
+++ index.js
@@ -2,11 +2,39 @@
 const inquirer = require('inquirer'); // npm import
 const mysql = require('mysql2'); // MySQL2 import
 
-// Creating a mysql connection
+// Connect to MySQL database
 const connection = mysql.createConnection({
     host:'localhost',
     port: 3301,
     user: 'root', // MySQL username
     password: '', // MySQL password
     database: 'employeeTracker' // Mysql database name
-})
+});
+
+connection.connect ((err) => {
+    if (err) throw err;
+    console.log('Connected to the Employee Tracker database!');
+    // Start the application
+    prompt();
+});
+
+// Prompt function created to prompt user to select from the choices provided
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'choices',
+            type: 'list',
+            message: 'Please select an option from those provided.'
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add A Department',
+                'Add A Role',
+                'Add An Employee',
+                'Update An Employee Role',
+                'Exit'
+            ],
+        })
+}
+
