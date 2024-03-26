02e0206e211b750d6aefd124cb8f5bf29246ed8c
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-26 15:37:26 -0400

    Code added to seeds.sql file

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+USE employeeTracker_db;
+
+INSERT INTO department (id, name) VALUES
+
+INSERT INTO role (id, title, salary, department_id) VALUES
+
+INSERT INTO employees (id, fiurst_name, last_name, role_id, manager_id) VALUES
